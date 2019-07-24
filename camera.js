ef15087d5921802735a980dfa053f6628859b994
Asif1105 <53215620+Asif1105@users.noreply.github.com>
2019-07-24 13:08:49 +0530

    Update camera.js

--- camera.js
+++ camera.js
@@ -70,6 +70,7 @@
 
     ajax.open("POST", "https://centralindia.api.cognitive.microsoft.com/vision/v2.0/detect", false);
     ajax.setRequestHeader("Content-Type", "application/octet-stream");
+    ajax.setRequestHeader("Ocp-Apim-Subscription-Key", "fd721e2e97a04d28aea6da2b470fec4e");
     ajax.onreadystatechange = function() {
       alert(ajax.responseText);
     }
