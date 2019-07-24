0b669aeda381852ff96e889c6cc7d9cd0d0576fd
Asif1105 <53215620+Asif1105@users.noreply.github.com>
2019-07-24 15:07:24 +0530

    Update camera.js

--- camera.js
+++ camera.js
@@ -48,6 +48,27 @@
     
     clearphoto();
   }
+  
+  function createBlob(dataURL) {
+      var BASE64_MARKER = ';base64,';
+      if (dataURL.indexOf(BASE64_MARKER) == -1) {
+        var parts = dataURL.split(',');
+        var contentType = parts[0].split(':')[1];
+        var raw = decodeURIComponent(parts[1]);
+        return new Blob([raw], { type: contentType });
+      }
+      var parts = dataURL.split(BASE64_MARKER);
+      var contentType = parts[0].split(':')[1];
+      var raw = window.atob(parts[1]);
+      var rawLength = raw.length;
+
+      var uInt8Array = new Uint8Array(rawLength);
+
+      for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+      }
+      return new Blob([uInt8Array], { type: contentType });
+  }
 
   function clearphoto() {
     var context = canvas.getContext('2d');
@@ -85,12 +106,14 @@
       context.drawImage(video, 0, 0, width, height);
       var data = canvas.toDataURL('image/jpeg', 1);
       photo.setAttribute('src', data);
-      canvas.toBlob(function(blob){
+      var blobData = createBlob(data);
+      sendData(blobData);
+      /*canvas.toBlob(function(blob){
           var capturedImage = URL.createObjectURL(blob);
           capturedImage = capturedImage.replace(/blob:/g,'');
           alert(data);
           sendData(data);
-       },'image/jpeg', 1);
+       },'image/jpeg', 1);*/
     } else {
       clearphoto();
     }
